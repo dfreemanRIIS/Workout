7bb564a9b4fa084c98357f5569d372f95ffa4878
dfreemanRIIS <dfreeman@riis.com>
2017-01-17 10:58:10 -0500

    Wire up fragment to display info from default values

--- app/src/main/java/com/example/dfreeman/workout/WorkoutDetailFragment.java
+++ app/src/main/java/com/example/dfreeman/workout/WorkoutDetailFragment.java
@@ -5,6 +5,7 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class WorkoutDetailFragment extends Fragment {
     private long workoutId;
@@ -12,6 +13,19 @@ public class WorkoutDetailFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_workout_detail, container, false);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        View view = getView();
+        if (view != null) {
+            TextView title = (TextView) view.findViewById(R.id.textTitle);
+            Workout workout = Workout.workouts[(int) workoutId];
+            title.setText(workout.getName());
+            TextView description = (TextView) view.findViewById(R.id.textDescription);
+            description.setText(workout.getDescription());
+        }
     }
 
     public void setWorkout(long id) {
