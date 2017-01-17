40bcb0170eefc979a9f7bbd1b4ab268def383002
dfreemanRIIS <dfreeman@riis.com>
2017-01-17 10:52:15 -0500

    Connect fragment

--- app/src/main/java/com/example/dfreeman/workout/WorkoutDetailFragment.java
+++ app/src/main/java/com/example/dfreeman/workout/WorkoutDetailFragment.java
@@ -1,15 +1,20 @@
 package com.example.dfreeman.workout;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 public class WorkoutDetailFragment extends Fragment {
+    private long workoutId;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_workout_detail, container, false);
+    }
+
+    public void setWorkout(long id) {
+        this.workoutId = id;
     }
 }
