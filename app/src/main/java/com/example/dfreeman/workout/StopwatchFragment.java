9c1854993ea619e51372d76a9ed662bc1e39f1ee
dfreemanRIIS <dfreeman@riis.com>
2017-01-18 08:35:26 -0500

    Android studio sucks

---
+++ app/src/main/java/com/example/dfreeman/workout/StopwatchFragment.java
@@ -0,0 +1,108 @@
+package com.example.dfreeman.workout;
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+import android.os.Handler;
+
+
+
+
+public class StopwatchFragment extends Fragment {
+
+    //initialize counters
+    private int seconds = 0;
+    private boolean running;
+    private boolean wasRunning;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View layout = inflater.inflate(R.layout.fragment_stopwatch, container, false);
+        runTimer(layout);
+        return layout;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if(savedInstanceState != null) {
+            seconds = savedInstanceState.getInt("seconds");
+            running = savedInstanceState.getBoolean("running");
+            wasRunning = savedInstanceState.getBoolean("wasRunning");
+        }
+    }
+
+    public void onClickStart(View view) {
+        running = true;
+    }
+
+    public void onClickStop(View view) {
+        running = false;
+    }
+
+    public void onClickReset(View view) {
+        running = false;
+        seconds = 0;
+    }
+
+    private void runTimer(View view) {
+        final TextView timeView = (TextView)view.findViewById(R.id.time_view);
+        final Handler handler = new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                int hours = seconds/3600;
+                int minutes = (seconds%3600)/60;
+                int secs = seconds%60;
+                String time = String.format("%d:%02d:%02d",hours,minutes,secs);
+                timeView.setText(time);
+                if(running) {
+                    seconds++;
+                }
+                handler.postDelayed(this, 1000);
+            }
+        });
+    }
+
+    @Override
+    public void onSaveInstanceState (Bundle savedInstanceState) {
+        savedInstanceState.putInt("seconds", seconds);
+        savedInstanceState.putBoolean("running", running);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        wasRunning = running;
+        running = false;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        if (wasRunning) {
+            running = true;
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        wasRunning = running;
+        running = false;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (wasRunning) {
+            running = true;
+        }
+    }
+}
+
